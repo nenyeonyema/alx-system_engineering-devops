6f231eadfba22a55cc2a36696cd9197ae41f8bf9
nenyeonyema <chinenyeonyema1@gmail.com>
2024-01-22 15:50:56 -0800

    create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create Puppet file with content
+
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
