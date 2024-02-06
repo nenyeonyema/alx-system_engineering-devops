c36b79c7886561d0a4dc4eec91597684c35a51fd
nenyeonyema <chinenyeonyema1@gmail.com>
2024-02-06 00:49:44 -0800

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,39 +1,30 @@
-# Install nginx package
-
+# Define package management for apt
 package { 'nginx':
   ensure => installed,
 }
 
-# Define a custom file for the nginx configuration
-file { '/etc/nginx/sites-available/custom_header':
-  ensure  => present,
-  content => "
-    server {
-      listen 80 default_server;
-      listen [::]:80 default_server;
-
-      server_name _;
-
-      location / {
-        add_header X-Served-By $hostname;
-        root /var/www/html;
-        index index.html index.htm;
-      }
-    }
-  ",
-  notify  => Service['nginx'],
-}
-
-# Create a symlink to enable the site
-file { '/etc/nginx/sites-enabled/custom_header':
-  ensure => link,
-  target => '/etc/nginx/sites-available/custom_header',
-  notify => Service['nginx'],
-}
-
-# Ensure nginx service is running and enabled
+# Ensure Nginx service is running
 service { 'nginx':
   ensure  => running,
   enable  => true,
   require => Package['nginx'],
+}
+
+# Define the Nginx site configuration
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => template('nginx/default.erb'),
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Define the index.html file content
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => "Hello World!\n",
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0644',
 }
