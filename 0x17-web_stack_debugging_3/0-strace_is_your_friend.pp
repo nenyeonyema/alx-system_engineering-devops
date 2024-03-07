d80664fefc621967c111f789a628f94ecdbca628
nenyeonyema <chinenyeonyema1@gmail.com>
2024-03-06 23:05:40 -0800

    updated

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,13 @@
-
-# Other necessary functions and logic
-# Othefest to automate fixing Apache 500 error
+# Puppet manifest to fix the web stack: Apache is returning a 200 and serving the correct page
 
 # Ensure that the Apache service is running
 service { 'apache2':
   ensure => 'running',
+}
+
+# Ensure that the Apache service starts on boot
+service { 'apache2':
+  enable => true,
 }
 
 # Fix permissions on Apache log files
@@ -14,9 +17,17 @@ file { '/var/log/apache2/error.log':
   mode   => '0644',
 }
 
+# Ensure the default Apache page is served
+file { '/var/www/html/index.html':
+  content => "<html><head><title>Welcome to Puppet Apache Server</title></head><body><h1>It works!</h1><p>This is the default web page for Puppet Apache server.</p></body></html>",
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0644',
+}
+
 # Restart Apache service to apply changes
 exec { 'restart_apache':
   command     => '/bin/systemctl restart apache2',
   refreshonly => true,
-  subscribe   => File['/var/log/apache2/error.log'],
+  subscribe   => File['/var/log/apache2/error.log', '/var/www/html/index.html'],
 }
