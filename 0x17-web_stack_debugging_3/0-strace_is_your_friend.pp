2dacf9f6b1564ac3465628fd3314b019b7825b78
nenyeonyema <chinenyeonyema1@gmail.com>
2024-03-10 04:59:26 -0700

    puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,32 +1,6 @@
-# Puppet manifest to fix the web stack: Apache is returning a 200 and serving the correct page
-# Ensure that the Apache service is running
-service { 'apache2':
-  ensure => 'running',
-}
+# Puppet manifest to fix a bug in wp-setings.php
 
-# Ensure that the Apache service starts on boot
-service { 'apache2':
-  enable => true,
-}
-
-# Fix permissions on Apache log files
-file { '/var/log/apache2/error.log':
-  owner => 'www-data',
-  group => 'www-data',
-  mode  => '0644',
-}
-
-# Ensure the default Apache page is served
-file { '/var/www/html/index.html':
-  content => "<html><head><title>Welcome to Puppet Apache Server</title></head><body><h1>It works!</h1><p>This is the default web page for Puppet Apache server.</p></body></html>",
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-}
-
-# Restart Apache service to apply changes
-exec { 'restart_apache':
-  command     => '/bin/systemctl restart apache2',
-  refreshonly => true,
-  subscribe   => File['/var/log/apache2/error.log', '/var/www/html/index.html'],
+exec { 'fix the php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
